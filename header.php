faf6ecdfa3278d5b455a425f934af52e4632a1e1
Necann <1199598@student.roc-nijmegen.nl>
2022-03-18 10:06:05 +0100

    improved header

---
+++ header.php
@@ -0,0 +1,36 @@
+<header>
+    <img class="sushilogo" src="afbeeldingen/sushilogo2.png" alt="Sushi Logo">
+    <div class="container-homepage" id="homepage">
+        <nav>
+            <ul>
+                <li><a href="#homepage"><div class="button1">HOME</div></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="button">
+        <div class="container-menu">
+            <nav>
+                <ul>
+                    <li><a href="#menu"><div class="button1">MENU</div></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="container-order">
+            <nav>
+                <ul>
+                    <li><a href="#order"><div class="button1">ORDER</div></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="container-contact">
+            <nav>
+                <ul>
+                    <li><a href="#contact"><div class="button1">CONTACT</div></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="shoppingcart">
+        <img class="shoppingcart1" src="/afbeeldingen/shoppingcart.png">
+    </div>
+</header>
